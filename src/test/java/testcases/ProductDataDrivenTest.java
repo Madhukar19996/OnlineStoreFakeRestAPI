62aba6401a63de590d0a874956f4f97b26a8bd98
Madhukar <madhukar19996@gmail.com>
2025-07-21 09:12:19 +0530

    First commit on 20-July-2025

--- src/test/java/testcases/ProductDataDrivenTest.java
+++ src/test/java/testcases/ProductDataDrivenTest.java
@@ -15,9 +15,51 @@ import routes.Routes;
 public class ProductDataDrivenTest extends Baseclass 
 
 {
+	
+	
+	@Test(dataProvider="jsonDataProvider", dataProviderClass=utils.DataProviders.class)
+	public void testAddNewProductWithJson(Map<String,String> data)
+	{
+		
+		String title=data.get("title");
+		double price=Double.parseDouble(data.get("price"));
+		String category=data.get("category");
+		String description=data.get("description");
+		String image=data.get("image");
+		
+		//String title, double price, String description, String image, String category
+		Product newProduct=new Product(title,price,description,image,category);
+		
+		
+		int productId=given()
+			.contentType(ContentType.JSON)
+			.body(newProduct)
+			
+		.when()
+			.post(Routes.CREATE_PRODUCT)
+		.then()
+			.log().body()
+			.statusCode(200)
+			.body("id", notNullValue())
+			.body("title", equalTo(newProduct.getTitle()))
+			.extract().jsonPath().getInt("id"); //Extracting Id from response body
+		
+		System.out.println("Product ID======> "+ productId);
+		
+		//Delete product
+		given()
+			.pathParam("id",productId)
+		.when()
+			.delete(Routes.DELETE_PRODUCT)
+		.then()
+			.statusCode(200);
+		
+		System.out.println("Deleted Product ID======> "+ productId);
+	}
+	
 
 	@Test(dataProvider = "jsonDataProvider", dataProviderClass = utils.DataProviders.class)
-	public void testAddNewProduct(Map<String, String> data) {
+	public void testAddNewProductWithCSV(Map<String, String> data) {
 	   /* String idStr = data.get("id");
 	    if (idStr == null || idStr.trim().isEmpty()) {
 	        throw new IllegalArgumentException("Missing or empty 'id' value in test data");
